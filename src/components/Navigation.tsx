b4e4530b051e3f8c16cb6cd73e892f35a58d4296
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-18 18:30:02 +0900

    style: Project Adjustment Design

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,9 +5,24 @@ const Navigation = () => {
   return (
     <div>
       <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink to="/about">About</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "font-bold text-blue-500" : "text-gray-500")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={({ isActive }) => (isActive ? "font-bold text-blue-500" : "text-gray-500")}
+        >
+          Products
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "font-bold text-blue-500" : "text-gray-500")}
+        >
+          About
+        </NavLink>
       </nav>
     </div>
   );
